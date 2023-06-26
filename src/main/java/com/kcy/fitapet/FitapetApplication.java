e94993ced3f9b32a059be2953a672165ea0a38a0
JaeSeo Yang <qud1251@gmail.com>
2023-06-26 20:52:10 +0900

    :tada: init: first commit

---
+++ src/main/java/com/kcy/fitapet/FitapetApplication.java
@@ -0,0 +1,13 @@
+package com.kcy.fitapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitapetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcy/fitapet/FitapetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcy.fitapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
