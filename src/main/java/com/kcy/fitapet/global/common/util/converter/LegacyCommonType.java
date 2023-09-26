3c5231509df594387fe174c4159cc1c414a5d987
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2023-09-26 13:11:06 +0900

    #19 enum converter

---
+++ src/main/java/com/kcy/fitapet/global/common/util/converter/LegacyCommonType.java
@@ -0,0 +1,9 @@
+package com.kcy.fitapet.global.common.util.converter;
+
+public interface LegacyCommonType {
+    /**
+     * Legacy Super System 공통 코드를 반환한다.
+     * @return String 공통 코드
+     */
+    String getCode();
+}
--- src/main/java/com/kcy/fitapet/global/config/SpringConfig.java
+++
@@ -1,7 +0,0 @@
-package com.kcy.fitapet.global.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfig {
-}
