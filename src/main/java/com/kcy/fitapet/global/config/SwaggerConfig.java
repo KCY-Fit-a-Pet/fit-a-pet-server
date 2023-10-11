daca4fd5b278057c1e95913531fb1f55b6211e82
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2023-10-11 18:15:06 +0900

    feat: #27 swagger server activeProfile 설정 추가

--- src/main/java/com/kcy/fitapet/global/config/SwaggerConfig.java
+++ src/main/java/com/kcy/fitapet/global/config/SwaggerConfig.java
@@ -1,25 +1,42 @@
 package com.kcy.fitapet.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ObjectUtils;
 
 @Configuration
+@OpenAPIDefinition(
+    servers = {
+            @Server(url = "http://localhost:8082", description = "Local Server"),
+            @Server(url = "${fitapet.domain.backend}", description = "Production Server")
+    }
+)
+@RequiredArgsConstructor
 public class SwaggerConfig {
     private static String JWT = "jwtAuth";
+    private final Environment environment;
 
     @Bean
     public OpenAPI openAPI() {
+        String activeProfile = "";
+        if (!ObjectUtils.isEmpty(environment.getActiveProfiles()) && environment.getActiveProfiles().length >= 1) {
+            activeProfile = environment.getActiveProfiles()[0];
+        }
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(JWT);
 
         return new OpenAPI()
-                .info(apiInfo())
-                .addServersItem(new Server().url(""))
+                .info(apiInfo(activeProfile))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url(""))
                 .addSecurityItem(securityRequirement)
                 .components(securitySchemes());
     }
@@ -37,9 +54,9 @@ public class SwaggerConfig {
                 .addSecuritySchemes(JWT, securitySchemeAccessToken);
     }
 
-    private Info apiInfo() {
+    private Info apiInfo(String activeProfile) {
         return new io.swagger.v3.oas.models.info.Info()
-                .title("Fit a Pet API")
+                .title("Fit a Pet API (" + activeProfile + ")")
                 .description("Fit a Pet Backend API Docs")
                 .version("1.0.0");
     }
