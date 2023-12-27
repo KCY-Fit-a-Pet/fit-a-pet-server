4533335a3846bb6658e41574799dd88b2ca88df0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2023-12-27 20:01:58 +0900

    feat: #11 OauthClientMapper && OauthApplicationConfigMapper 반영

--- src/main/java/com/kcy/fitapet/global/common/security/oauth/OauthApplicationConfigMapper.java
+++ src/main/java/com/kcy/fitapet/global/common/security/oauth/OauthApplicationConfigMapper.java
@@ -4,6 +4,8 @@ import com.kcy.fitapet.domain.oauth.exception.OauthException;
 import com.kcy.fitapet.domain.oauth.type.ProviderType;
 import com.kcy.fitapet.global.common.response.exception.GlobalErrorException;
 import com.kcy.fitapet.global.common.security.oauth.OauthApplicationConfig;
+import com.kcy.fitapet.global.common.security.oauth.apple.AppleApplicationConfig;
+import com.kcy.fitapet.global.common.security.oauth.google.GoogleApplicationConfig;
 import com.kcy.fitapet.global.common.security.oauth.kakao.KakaoApplicationConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,13 +14,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OauthApplicationConfigMapper {
     private final KakaoApplicationConfig kakaoApplicationConfig;
+    private final GoogleApplicationConfig googleApplicationConfig;
+    private final AppleApplicationConfig appleApplicationConfig;
 
     public OauthApplicationConfig getOauthApplicationConfig(ProviderType provider) {
         return switch (provider) {
             case KAKAO -> kakaoApplicationConfig;
-            case GOOGLE -> null;
-            case APPLE -> null;
-            case NAVER -> null;
+            case GOOGLE -> googleApplicationConfig;
+            case APPLE -> appleApplicationConfig;
             default -> throw new GlobalErrorException(OauthException.INVALID_PROVIDER);
         };
     }
--- src/main/java/com/kcy/fitapet/global/common/security/oauth/OauthClientMapper.java
+++ src/main/java/com/kcy/fitapet/global/common/security/oauth/OauthClientMapper.java
@@ -4,6 +4,8 @@ import com.kcy.fitapet.domain.oauth.exception.OauthException;
 import com.kcy.fitapet.domain.oauth.type.ProviderType;
 import com.kcy.fitapet.global.common.response.exception.GlobalErrorException;
 import com.kcy.fitapet.global.common.security.oauth.OauthClient;
+import com.kcy.fitapet.global.common.security.oauth.apple.AppleOauthClient;
+import com.kcy.fitapet.global.common.security.oauth.google.GoogleOauthClient;
 import com.kcy.fitapet.global.common.security.oauth.kakao.KakaoOauthClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OauthClientMapper {
     private final KakaoOauthClient kakaoOauthClient;
+    private final GoogleOauthClient googleOauthClient;
+    private final AppleOauthClient appleOauthClient;
 
     public OauthClient getOauthClient(ProviderType provider) {
         return switch (provider) {
             case KAKAO -> kakaoOauthClient;
-            case GOOGLE -> null;
-            case APPLE -> null;
+            case GOOGLE -> googleOauthClient;
+            case APPLE -> appleOauthClient;
             default -> throw new GlobalErrorException(OauthException.INVALID_PROVIDER);
         };
     }
