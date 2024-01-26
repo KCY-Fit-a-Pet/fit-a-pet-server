1147ef45949828af110a7bcefc41ce6d9b6d1014
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-01-26 15:58:15 +0900

    feat: #83 JPAQuery 구성 메서드 정의

--- src/main/java/com/kcy/fitapet/global/common/repository/DefaultSearchQueryDslRepositoryImpl.java
+++ src/main/java/com/kcy/fitapet/global/common/repository/DefaultSearchQueryDslRepositoryImpl.java
@@ -1,19 +1,22 @@
 package com.kcy.fitapet.global.common.repository;
 
-import com.querydsl.core.types.EntityPath;
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.*;
 import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.querydsl.QSort;
 import org.springframework.data.querydsl.SimpleEntityPathResolver;
+import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class DefaultSearchQueryDslRepositoryImpl<T> implements DefaultSearchQueryDslRepository<T> {
     private final EntityManager em;
@@ -49,6 +52,50 @@ public class DefaultSearchQueryDslRepositoryImpl<T> implements DefaultSearchQuer
 
     @Override
     public <P> Page<P> selectPage(Predicate predicate, Class<P> type, Map<String, Expression<?>> bindings, QueryHandler queryHandler, Pageable pageable) {
-        return null;
+        Assert.notNull(pageable, "pageable must not be null!");
+
+
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private JPAQuery<?> buildWithoutSelect(Predicate predicate, Map<String, Expression<?>> bindings, QueryHandler queryHandler, Sort sort) {
+        JPAQuery<?> query = queryFactory.from(path);
+
+        if (predicate != null) query = query.where(predicate);
+        if (queryHandler != null) query = queryHandler.apply(query);
+        if (sort != null) {
+            if (sort instanceof QSort qSort) {
+                query = query.orderBy(qSort.getOrderSpecifiers().toArray(new OrderSpecifier[0]));
+            } else {
+                Function<Sort.NullHandling, OrderSpecifier.NullHandling> castToQueryDsl = nullHandling -> switch (nullHandling) {
+                    case NATIVE -> OrderSpecifier.NullHandling.Default;
+                    case NULLS_FIRST -> OrderSpecifier.NullHandling.NullsFirst;
+                    case NULLS_LAST -> OrderSpecifier.NullHandling.NullsLast;
+                };
+
+                for (Sort.Order order : sort) {
+                    OrderSpecifier.NullHandling queryDslNullHandling = castToQueryDsl.apply(order.getNullHandling());
+
+                    Order orderBy = order.isAscending() ? Order.ASC : Order.DESC;
+                    OrderSpecifier<?> os;
+
+                    if (bindings != null && bindings.containsKey(order.getProperty())) {
+                        Expression<?> expression = bindings.get(order.getProperty());
+
+                        if (expression instanceof Operation && ((Operation<?>) expression).getOperator() == Ops.ALIAS) {
+                            os = new OrderSpecifier<>(orderBy, Expressions.stringPath(((Operation<?>) expression).getArg(1).toString()), queryDslNullHandling);
+                        } else {
+                            os = new OrderSpecifier(orderBy, expression, queryDslNullHandling);
+                        }
+                    } else {
+                        os = new OrderSpecifier<>(orderBy, Expressions.stringPath(order.getProperty()), queryDslNullHandling);
+                    }
+
+                    query = query.orderBy(os);
+                }
+            }
+        }
+
+        return query;
     }
 }
