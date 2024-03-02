0a3ac58c146ac0e66f313b1cd568500c4de14d4e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-02 19:44:17 +0900

    feat: #120 notification api

---
+++ fitapet-app-external-api/src/main/java/kr/co/fitapet/api/apis/notification/usecase/NotificationUseCase.java
@@ -0,0 +1,11 @@
+package kr.co.fitapet.api.apis.notification.usecase;
+
+import kr.co.fitapet.common.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UseCase
+@RequiredArgsConstructor
+public class NotificationUseCase {
+}
